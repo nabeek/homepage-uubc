0bb4f240788c64f13fe5e2a5159387d30817d67e
nabeek <nabeek@gmail.com>
2020-05-28 15:09:35 -0600

    Updated portfolio with multiple projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,63 @@
 // Burger menu functionality
-$(document).ready(function() {
-
-    $(".navbar-burger").click(function() {
-  
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        $(".navbar-burger").toggleClass("is-active");
-        $(".navbar-menu").toggleClass("is-active");
-  
-    });
+$(document).ready(function () {
+  $(".navbar-burger").click(function () {
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
   });
+});
 
 // Portfolio item event listener
-const project = $(".portfolio-project");
+let project = $(".portfolio-project");
 
-project.click(function() {
-  let project = this.textContent;
+project.click(function () {
+  let name = this.textContent;
   let repo = $("#portfolio-repo");
   let link = $("#portfolio-link");
   let screenshot = $("#portfolio-screenshot");
+  console.log(name);
 
-  $("#portfolio-right-name").text(project);
+  $("#portfolio-right-name").text(name);
   repo.show();
   link.show();
   screenshot.show();
 
-  if (project === "Earthquake Now") {
+  if (name === "Earthquake Now") {
     screenshot.attr("src", "./assets/images/en screenshot.png");
     repo.attr("href", "https://github.com/nabeek/Earthquake-Now");
     link.attr("href", "https://nabeek.github.io/Earthquake-Now/");
-  } else if (project === "JavaScript Quiz") {
+  } else if (name === "JavaScript Quiz") {
     screenshot.attr("src", "./assets/images/quiz screenshot.png");
     repo.attr("href", "https://github.com/nabeek/UUBC-Homework4");
     link.attr("href", "https://nabeek.github.io/UUBC-Homework4/");
-  } else if (project === "Daily Planner") {
+  } else if (name === "Daily Planner") {
     screenshot.attr("src", "./assets/images/planner screenshot.png");
     repo.attr("href", "https://github.com/nabeek/UUBC-Homework5");
     link.attr("href", "https://nabeek.github.io/UUBC-Homework5/");
+  } else if (name === "Team Generator") {
+    screenshot.attr(
+      "src",
+      "https://user-images.githubusercontent.com/4752937/81086301-87652900-8eb5-11ea-82fb-34cf76147439.png"
+    );
+    repo.attr("href", "https://github.com/nabeek/employee-summary");
+    link.hide();
+  } else if (name === "Note Taker") {
+    screenshot.attr("src", "./assets/images/note-taker screenshot.png");
+    repo.attr("href", "https://github.com/nabeek/note-taker");
+    link.attr("href", "https://nabeek-note-taker.herokuapp.com/");
+  } else if (name === "Employee Tracker") {
+    screenshot.attr(
+      "src",
+      "https://user-images.githubusercontent.com/4752937/82268960-77c1f780-992d-11ea-9b0c-28017c7fd026.png"
+    );
+    repo.attr("href", "https://github.com/nabeek/employee-tracker");
+    link.hide();
+  } else if (name === "Eat-Da-Burger") {
+    screenshot.attr("src", "./assets/images/eat-da-burger screenshot.png");
+    repo.attr("href", "https://github.com/nabeek/eat-da-burger");
+    link.attr("href", "https://nabeek-eat-da-burger.herokuapp.com/");
   }
 });
 
-
 // Auto-update copyright year
-$("#copyright-year").text(moment().format("YYYY"))
+$("#copyright-year").text(moment().format("YYYY"));
